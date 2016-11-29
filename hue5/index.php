a9b8b3ff6d24d843e255e194e6cdec8b44c73855
mendo2001 <dmeneder@gmail.com>
2016-11-29 10:44:02 +0100

    Create index.php

---
+++ hue5/index.php
@@ -0,0 +1,36 @@
+<html>
+  <head>
+  	<meta charset="UTF-8">
+	<title>Hausübung 5 | EXPLODE</title>
+	<style>
+
+	.button{
+		padding: 10px;
+	}
+	</style>
+  </head>
+  <body>
+		<form method="post" action="//localhost/medt/hue5/index.php">
+			<label>Eingabe: <input type="text" name="eingabe" /></label>
+			<div class="button">
+				<input type="submit" name="resetBtn" value="RESET">
+				<input type="submit" name="explodeBtn" value="EXPLODE">
+			</div>
+		</form>
+		<p> Ihre Eingabe als Liste </p>
+		<?php
+			$text =  $_POST['eingabe'];
+
+				if(isset($_POST['explodeBtn'])){ ?>
+					<?php
+						$Array = explode(" ",$text);
+						echo "<ul>";
+						foreach($Array as $item)
+						{
+							echo "<li>$item</li>";
+						}
+						echo "</ul>";
+					?>
+		<?php } ?>
+  </body>
+</html>
